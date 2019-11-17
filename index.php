677d1de516d7bd3437b456db34b998ae67def792
ChebilAmine <amine.chebil@iit.ens.tn>
2019-11-17 16:21:58 +0100

    amine

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		        <a class="active" href="index.php">Home</a>
 		      </li>
 		      <li>
-		        <a href="shop.php">Shop</a>
+		        <a href="shop.php">Shopa</a>
 		      </li>
 		      <?php if (!isset($_SESSION['customer_email'])): ?>
 						<li><a href="checkout.php">My Account</a></li>
