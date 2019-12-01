fb6621f483ccf70b87714b5e1820f2a4eef38b07
Mohamed Mseddi <mameya.mseddi@gmail.com>
2019-12-01 21:39:34 +0100

    fix

--- includes/show_all_products.php
+++ includes/show_all_products.php
@@ -1,9 +1,9 @@
-<div class="card mb-3">
+<!-- <div class="card mb-3">
   <div class="card-body">
     <h5 class="card-title">Shop</h5>
-    <!-- <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus neque nisi cupiditate, eligendi molestias corporis nihil ipsa distinctio in repellendus!</!-->
+    <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus neque nisi cupiditate, eligendi molestias corporis nihil ipsa distinctio in repellendus!</!
   </div>
-</div>
+</div> -->
 
 
 <div class="row" id="Products">
